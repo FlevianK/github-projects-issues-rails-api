ecacedcc72ac9e586e9718698acc6912c8f80f5f
FlevianK <flevian.kanaiza@andela.com>
2018-09-29 16:00:59 +0300

    Update project locally

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,23 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:update]
+
   def create
     @project = Project.create!(project_params)
     json_response(@project, :created)
+  end
+
+  def update
+    @project.update(project_params)
+    json_response(@project, :updated)
   end
 
   private
 
   def project_params
     params.require(:project).permit(:project, :repo)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 end
