aacda46916155b9ff197edf49f84c13e8cd82e46
FlevianK <flevian.kanaiza@andela.com>
2018-09-29 16:03:44 +0300

    Update issue locally

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,12 +1,23 @@
 class IssuesController < ApplicationController
+  before_action :set_issue, only: [:update]
+
   def create
     @issue = Issue.create!(issue_params)
     json_response(@issue, :created)
+  end
+
+  def update
+    @issue.update(issue_params)
+    json_response(@issue, :updated)
   end
 
   private
 
   def issue_params
     params.require(:issue).permit(:issue)
+  end
+
+  def set_issue
+    @issue = Issue.find(params[:id])
   end
 end
