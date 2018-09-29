33af56dc41f0a04056ccbe70946fdde94e18be4d
FlevianK <flevian.kanaiza@andela.com>
2018-09-29 15:51:54 +0300

    Create issue locally

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,2 +1,12 @@
 class IssuesController < ApplicationController
+  def create
+    @issue = Issue.create!(issue_params)
+    json_response(@issue, :created)
+  end
+
+  private
+
+  def issue_params
+    params.require(:issue).permit(:issue)
+  end
 end
