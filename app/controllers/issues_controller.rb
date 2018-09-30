fea8e3cf87d3c5c0909d9cf78928036a702c4338
FlevianK <flevian.kanaiza@andela.com>
2018-09-30 20:18:20 +0300

    create issue in git

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,8 +1,11 @@
 class IssuesController < ApplicationController
   before_action :set_issue, only: [:update]
+  before_action :set_url
 
   def create
-    @issue = Issue.create!(issue_params)
+    res = JSON.parse(GithubConnection.create(@url, issue_params).body)
+    issue = {:issue => res["id"]}
+    @issue = Issue.create!(issue)
     json_response(@issue, :created)
   end
 
@@ -14,10 +17,14 @@ class IssuesController < ApplicationController
   private
 
   def issue_params
-    params.require(:issue).permit(:issue)
+    params.require(:issue).permit(:title, :body)
   end
 
   def set_issue
     @issue = Issue.find(params[:id])
+  end
+
+  def set_url
+    @url = "#{GithubConnection::URL}/issues"
   end
 end
